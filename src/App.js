1f0193eb510bc74df23326c66480944f272c60d9
ashu180674 <ashutripathi16.10.2001@gmail.com>
2024-06-07 20:36:00 +0530

    have a nice day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Services from './components/Services';
+import ContactForm from './components/ContactForm';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Services />
+      <ContactForm />
     </div>
   );
-}
+};
 
 export default App;
